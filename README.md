8e3e7a3dd5ce79987aecf1143cbb078dbb6bfd22
jpfeiffer16 <jpfeiffer@bluemodus.com>
2017-05-22 16:20:46 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Regionifyer
+A little program that maps ip addresses to AWS regions
+
+## Usage
+
+`$ git clone <git ssh enpoint>`
+
+`$ cd regionifyer`
+
+`$ ./regionifyer.py <valid IPv4 address>`
+
+```
+us-west-2
+```
---
+++ regionifyer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import sys
+import urllib.request
+import json
+from datetime import datetime
+
+ERR_MSG = 'Must provide a valid ip'
+def crash():
+  print(ERR_MSG)
+  exit(1)
+
+ip = sys.argv[1]
+if type(ip) is not str: crash()
+octets = ip.split('.')
+if (len(octets) != 4): crash()
+
+jsonResult = urllib.request.urlopen("https://ip-ranges.amazonaws.com/ip-ranges.json").read().decode("utf-8")
+mappings = json.loads(jsonResult)
+dateChanged = datetime.strptime(mappings['createDate'], '%Y-%m-%d-%H-%M-%S')
+print('IP map last updated: ' + str(dateChanged))
+sys.stdout.write('Region is: ')
+
+for mapping in list(
+  filter(
+      lambda x: x['service'] == 'EC2' and x['ip_prefix'] != None,
+      mappings['prefixes']
+    )
+  ):
+  matchOctets = mapping['ip_prefix'].split('.')
+  if octets[0] == matchOctets[0] and octets[1] == matchOctets[1]:
+    print(mapping['region'])
