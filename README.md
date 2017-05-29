a37129220db571ab77df450a3df7e86a68c20353
jpfeiffer16 <jpfeiffer@bluemodus.com>
2017-05-29 11:45:53 -0600

    Adding more to docs

--- README.md
+++ README.md
@@ -12,3 +12,12 @@ A little program that maps ip addresses to AWS regions
 ```
 us-west-2
 ```
+
+## Implementation Details
+* Gets JSON from https://ip-ranges.amazonaws.com/ip-ranges.json
+* Runs through the ip list and sees if there is a direct match for ip.
+  * If there is then it uses that ip
+* If there is no direct ip match, it trys to match the 1st, and 2nd octets.
+  * If it cannot match this, fail.
+* If it can match that, it then finds out the two mappings with the 3rd octet greater and less than the 3rd octet of the given ip
+* Print out that it could be either of those mappings.
--- regionifyer.py
+++ regionifyer.py
@@ -9,11 +9,19 @@ def crash():
   print(ERR_MSG)
   exit(1)
 
-# def get_hi(mapping_to_match, mappings):
+def get_hi(mapping_to_match, mappings):
 
-#   hi = 255
-#   for mapping in mappings:
-#     if mapping['ip_prefix'].split('.')[2] < hi and mapping
+  # hi = 255
+  hi = {
+    'ip_prefix': "13.32.255.0/15",
+    'region': "GLOBAL",
+    'service': "AWS"
+  }
+  print(hi)
+  for mapping in mappings:
+    if (mapping['ip_prefix'].split('.')[2] < hi['ip_prefix'].split('.')[2]) and (mapping['ip_prefix'].split('.')[2] > mapping_to_match['ip_prefix'].split('.')[2]):
+      mapping = mapping
+  return hi
 
 ip = sys.argv[1]
 if type(ip) is not str: crash()
@@ -26,15 +34,17 @@ dateChanged = datetime.strptime(mappings['createDate'], '%Y-%m-%d-%H-%M-%S')
 print('IP map last updated: ' + str(dateChanged))
 sys.stdout.write('Region is: ')
 
-for mapping in list(
-  filter(
-      lambda x: x['service'] == 'EC2' and x['ip_prefix'] != None,
-      mappings['prefixes']
-    )
-  ):
+aws_mappings = list(
+filter(
+    lambda x: x['service'] == 'EC2' and x['ip_prefix'] != None,
+    mappings['prefixes']
+  )
+)
+
+for mapping in aws_mappings:
   matchOctets = mapping['ip_prefix'].split('.')
   if octets[0] == matchOctets[0] and octets[1] == matchOctets[1]:
     print(mapping['region'])
   else:
     #TODO: Fix it
-    print('Not found')
+    print(get_hi(mapping, mappings))
